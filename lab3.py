c28838b36394f27e645d966969b1b86d7b83d70e
sakshamgupta04 <121110080+sakshamgupta04@users.noreply.github.com>
2023-10-17 21:40:38 +0530

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,23 @@
+# Consider the string “Welcome to Python world”. Perform the following operations: Count the number of alphabets in the given string. To extract characters in the given, range from the given string. Check if the string is alphanumeric or not. 
+
+str="Welcome to Python World"
+print('Original string:',str)
+while(1):
+    print('1.Count no of alphabets')
+    print('2.Extract Character using range')
+    print('3.String is alphnumeric or not')
+    print('4.Exit')
+    c=int(input('Enter your choice'))
+    if(c==1):
+        l=len([ele for ele in str if ele.isalpha()])
+        print(l)
+    elif(c==2):
+        start=int(input('Enter start range'))
+        end=int(input('Enter end range'))
+        print(str[start:end])
+    elif(c==3):
+        print(str)
+        b=str.isalnum()
+        print(b)
+    elif(c==4):
+        break
